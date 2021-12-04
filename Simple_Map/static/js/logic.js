b19a91c5d1d603acd98ee6c6bcbf89905cd47a57
jj2773 <87623223+jj2773@users.noreply.github.com>
2021-12-04 07:58:07 -0600

    update mapping lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,10 +4,25 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
-    40.7, -94.5
+    37.6213, -122.3790
   ],
-  zoom: 4
+  zoom: 5
 });
+
+// preparing airline route info for LAX to SFO
+// it requires gps coordinates as an array to draw the lines on the map
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 let cityData=cities;
 
@@ -47,7 +62,7 @@ cityData.forEach(function(city) {
 
 // We create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
