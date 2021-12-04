01b4144af35aa20ac19a08ab859f34e51b129160
jj2773 <87623223+jj2773@users.noreply.github.com>
2021-12-04 07:36:54 -0600

    multiple points 13_4_2

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -9,10 +9,27 @@ let map = L.map("mapid", {
   zoom: 4
 });
 
+let cityData=cities;
 
+// Loop through the cities array and create one marker for each city.
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location,{
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
+//Single Point marker
 //  Add a marker to the map for Los Angeles, California.
-//approach 1
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+//  Add a circle marker to the map for Los Angeles, California.
+// approach 1 
+// measures circle in meters
 // var circle = L.circle([34.0522,-118.2437], {
 //   color: 'black',
 //   fillColor: '#ffffa1',
@@ -20,12 +37,13 @@ let map = L.map("mapid", {
 //   radius: 300
 // }).addTo(map);
 
-//approach 2
-L.circleMarker([34.0522, -118.2437],{
-radius:300,
-color: 'black',
-fillColor: '#ffffa1'
-}).addTo(map);
+// //approach 2
+// measures the circle in pixels 
+// L.circleMarker([34.0522, -118.2437],{
+// radius:300,
+// color: 'black',
+// fillColor: '#ffffa1'
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
