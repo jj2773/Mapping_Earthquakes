1b559fea8a87a05c47f4179cabb0c8344f63f8b0
jj2773 <87623223+jj2773@users.noreply.github.com>
2021-12-04 10:07:41 -0600

    start of 13_5_3

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,38 +4,104 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
-    37.6213, -122.3790
+    37.619, -122.375
   ],
-  zoom: 5
+  zoom: 13
 });
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+//example 1 plots a market at the airport coordinates
+// L.geoJSON(sanFranAirport).addTo(map);
+
+//For the pointToLayer callback function, the basic syntax for adding functionality to a marker is as follows:
+//example 2 plots marker on each feature
+// L.geoJson(sanFranAirport, {
+//   pointToLayer: function(feature, latlng) {
+//     return L.marker(latlng);
+//    }
+// }).addTo(map);
+
+
+//example 3 plots each feature
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng);
+//   }
+//   }).addTo(map);
+
+  //example 4 plots each feature with Popup
+  // L.geoJSON(sanFranAirport, {
+  //   // We turn each feature into a marker on the map.
+  //   pointToLayer: function(feature, latlng) {
+  //     console.log(feature);
+  //     return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>")
+  //   }
+  //   }).addTo(map);
+var layerjj=[]
+var featurejj=[]
+
+  //example 5 on each feature with Popup allows for adding properties of the object
+  //features are passed into the feature variable while properties are pasted in the layer variable
+  L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+      featurejj=feature
+      layerjj=layer
+      console.log(layer)
+      console.log(feature)
+      layer.bindPopup("<h2>" + feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + feature.properties.name+ "</h3>");
+      }
+}).addTo(map);
+
 
 // preparing airline route info for LAX to SFO
 // it requires gps coordinates as an array to draw the lines on the map
 // Coordinates for each point to be used in the line.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
 
 // Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
 
-let cityData=cities;
+// let cityData=cities;
 
 // Loop through the cities array and create one marker for each city.
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location,{
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location,{
+//     radius: city.population/100000
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
 
 //Single Point marker
 //  Add a marker to the map for Los Angeles, California.
